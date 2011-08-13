9bf35be476b57e0456c16e2a7741b2c4c0393c32
happybird <godtai@qq.com>
2011-08-13 10:11:54 +0800

    get the address of int and char and the next address
    p1+1,p2+1

--- 1.c
+++ 1.c
@@ -2,13 +2,19 @@
 
 int main(int argc, const char *argv[])
 {
-    int i;
-    int *p;
-    p = &i;
-    i = 1;
+    int i = 1;
+    char c = 1;
+    int *p1;
+    p1 = &i;
+    char *p2;
+    p2 = &c;
     printf("0x%x\n",&i);
-    printf("%d\n",*p);
-    *p = 2;
+    printf("%d\n",*p1);
+    *p1 = 2;
     printf("%d",i);
+    printf("0x%x\n",p1);
+    printf("0x%x\n",p1 + 1);
+    printf("0x%x\n",p2);
+    printf("0x%x\n",p2+1);
     return 0;
 }
